bc2c68d0957b32ff88afb137d62673409e91be94
Muhammad Gamal <dev.muhammad222000@gmail.com>
2021-01-20 17:26:57 +0200

    Models

---
+++ server/models/userModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+
+  { timestamps: true } // To make mongoose automatically manage createdAt and updatedAt properties
+);
+
+userSchema.methods.matchPassword = function (enteredPassword)  {
+    return await bcrypt.compare(enteredPassword, this.password)
+  }
+  
+
+userSchema.pre('save', function (next)  {
+   try {
+       const salt = await bcrypt.genSalt(10)
+       const hashedPassword = await bcrypt.hash(this.password, salt)
+
+       this.password = hashedPassword
+       next()
+   } catch (error) {
+       next(error)
+       
+   }
+})
+
+const User =mongoose.model('User', userSchema)
+
+export default User
